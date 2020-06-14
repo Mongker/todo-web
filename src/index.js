d62fe85c99825af43707ee03fcf1be05130e542d
Mongker <49313037+Mongker@users.noreply.github.com>
2020-06-14 23:18:50 +0700

    MongLV:
    Tạo khung code reactJS + redux + saga

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
+import App from './App';
+
+// UI ANT DESIGN
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+
+// Redux
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+
+// Redux-Saga
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./sagas/rootSaga";
+
+// Reducers
+import allReducers from "./reducers/allReducers";
+
+// Middleware
+const sagaMiddleware = createSagaMiddleware();
+
+// Store
+let store = createStore(allReducers, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  rootElement
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/reducers/allReducers.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+
+const allReducers = combineReducers({
+  // Nếu có nhiều reducers thì viết thêm ở dướidưới
+});
+export default allReducers;
